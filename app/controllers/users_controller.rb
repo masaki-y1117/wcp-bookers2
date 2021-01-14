5b85266249cb85b59e2991aa52f0313f29f7a647
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-14 12:40:41 +0000

    [Userモデル修正]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,9 @@ class UsersController < ApplicationController
   end
   
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   def destroy
