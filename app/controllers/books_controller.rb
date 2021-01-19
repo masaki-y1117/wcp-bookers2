b241a1888e1bd64db5729357004aaf38452d62d8
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-19 08:07:28 +0000

    [投稿一覧修正]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,12 @@ class BooksController < ApplicationController
   end
   
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path
+      redirect_to book_path(@book)
     else
       render :index
     end
@@ -17,7 +18,9 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
     @user = current_user
+    
   end
 
   def show
@@ -29,17 +32,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
+    else
+      render :edit
     end
   end
   
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to book_path
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
