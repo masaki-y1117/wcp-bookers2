0add32ee9bb2f918c3cc0c9aa388ebd738e10ae4
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-20 11:53:20 +0000

    [投稿画面修正]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
   
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
   
   def update
@@ -28,6 +32,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
